44001b52c3ca7cc8e478a1bd1d98fa5cc075d509
arnavkato <arnavkatoch47@gmail.com>
2025-05-10 14:50:23 -0400

    added draws

--- script.js
+++ script.js
@@ -31,30 +31,60 @@ function createGameFlow() {
   const gameBoard = createGameboard();
 
   function checkWinner(board) {
-            // Check rows
-            for (let i = 0; i < 3; i++) {
-                if (board[i][0] && board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
-                    return board[i][0];
-                }
-            }
-            
-            // Check columns
-            for (let i = 0; i < 3; i++) {
-                if (board[0][i] && board[0][i] === board[1][i] && board[1][i] === board[2][i]) {
-                    return board[0][i];
-                }
-            }
-            
-            // Check diagonals
-            if (board[0][0] && board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
-                return board[0][0];
-            }
-            if (board[0][2] && board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
-                return board[0][2];
-            }
-            
-            return null;
-        };
+    // Check rows
+    for (let i = 0; i < 3; i++) {
+      if (
+        board[i][0] &&
+        board[i][0] === board[i][1] &&
+        board[i][1] === board[i][2]
+      ) {
+        return board[i][0];
+      }
+    }
+
+    // Check columns
+    for (let i = 0; i < 3; i++) {
+      if (
+        board[0][i] &&
+        board[0][i] === board[1][i] &&
+        board[1][i] === board[2][i]
+      ) {
+        return board[0][i];
+      }
+    }
+
+    // Check diagonals
+    if (
+      board[0][0] &&
+      board[0][0] === board[1][1] &&
+      board[1][1] === board[2][2]
+    ) {
+      return board[0][0];
+    }
+    if (
+      board[0][2] &&
+      board[0][2] === board[1][1] &&
+      board[1][1] === board[2][0]
+    ) {
+      return board[0][2];
+    }
+
+    // Check for draw
+    let isDraw = true;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (board[i][j] === null) {
+          isDraw = false;
+          break;
+        }
+      }
+    }
+    if (isDraw) {
+      return "draw";
+    }
+
+    return null;
+  }
   
   function startGame() {
     let currentPlayer = players[0];
